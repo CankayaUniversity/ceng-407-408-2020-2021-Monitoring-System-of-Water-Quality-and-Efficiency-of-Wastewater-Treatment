df43a81e3c3d9464d7c9993025e566042eafbc87
akgvn <akgvn@yandex.com>
2021-05-11 18:32:06 +0300

    Some data engineering work for 2018 dataset.

--- source/data pipeline/excel_read/excel_reader.py
+++ source/data pipeline/excel_read/excel_reader.py
@@ -5,10 +5,13 @@
 import glob, pickle, os
 from openpyxl import Workbook, load_workbook
 
-from static_data import possible_row_names_2020, rows_with_data_2020
+from static_data import *
 
 import pprint # for debugging
 pp = pprint.PrettyPrinter(indent = 4) # for debug
+
+excel_files_2020 = glob.glob("./data/2020/*.xlsm") # Get a list of 2020 .xlsm files.
+excel_files_2018 = glob.glob("./data/2018/Excel Files/*.xlsx") # Get a list of 2018 .xlsx files.
 
 # Pickle utils --------------
 def load_from_pickle(name):
@@ -30,9 +33,15 @@ class ExcelDataReader(object):
         self.filename = os.path.basename(filepath)
         self.workbook = load_workbook(filepath, keep_vba = True, read_only = True)
         self.data_tables = []
-        self.current_worksheet = None
-        self.worksheet_dimensions = None # ((row, col), (row, col))
-        self.current_data_table = {}
+        self.current_worksheet    = None
+        self.worksheet_dimensions = None # ( Top-left (row, col), bottom-right (row, col) )
+        self.current_data_table   = {}
+
+    def __del__(self):
+        # This is only here because I'm curious whether Python objects get cleaned up deterministically. (in CPython)
+        # As far as I can tell, objects that go out of scope are deleted immediately. Maybe RC is used?
+        print("Destructor called for", self.filename)
+        self.close()
 
     def close(self):
         self.workbook.close()
@@ -42,7 +51,7 @@ class ExcelDataReader(object):
         intersection = [value for value in self.workbook.sheetnames if value in possible_sheetnames]
         return intersection
 
-    def open_datasheet(self, ws_name):
+    def open_worksheet(self, ws_name):
         self.current_worksheet = self.workbook[ws_name]
         
         # Dimension stuff...
@@ -62,6 +71,8 @@ class ExcelDataReader(object):
         self.worksheet_dimensions = ((top_left_col, top_left_row), (bottom_right_col, bottom_right_row))
 
     def get_row_names(self):
+        assert (self.current_worksheet is not None), ("Use open_worksheet! Worksheet is None in " + self.filename)
+
         row_names = []
         top_left_column = self.worksheet_dimensions[0][0]
         row_count = self.worksheet_dimensions[1][1]
@@ -69,40 +80,43 @@ class ExcelDataReader(object):
         for row_idx in range(1, row_count + 1):
             val = self.current_worksheet.cell(row = row_idx, column = top_left_column).value
             
+
             if val is not None:
                 row_names.append(val)
+                if val not in possible_row_names_2018:
+                    print("Row not in possible list:", val)
 
         return row_names
 
-    def read_row(self, row_idx, data_table):
+    def read_row_into_table(self, row_idx, data_table):
         top_left_column = self.worksheet_dimensions[0][0]
         column_name = self.current_worksheet.cell(row = row_idx, column = top_left_column).value
     
         if column_name == 'NUMUNE KODU:': # table start
-            data_temp = self.current_worksheet.cell(row = row_idx, column = 2 + 1).value
+            data_temp = self.current_worksheet.cell(row = row_idx, column = top_left_column + 1).value
             data_table["NUMUNE KODU"] = data_temp
             return 1 # Started
     
         if column_name not in possible_row_names_2020:
-            data_temp = self.current_worksheet.cell(row = row_idx, column = 2).value
+            data_temp = self.current_worksheet.cell(row = row_idx, column = top_left_column).value
             if data_temp is not None:
                 data_table["Açıklama"] = data_temp
             return -1 # Ended
     
         if column_name == 'BÖLGE ADI:':
-            data_temp = self.current_worksheet.cell(row = row_idx, column = 2 + 1).value
+            data_temp = self.current_worksheet.cell(row = row_idx, column = top_left_column + 1).value
             data_table["BÖLGE ADI"] = data_temp
     
         if column_name == 'YER:':
-            data_temp = self.current_worksheet.cell(row = row_idx, column = 2 + 1).value
+            data_temp = self.current_worksheet.cell(row = row_idx, column = top_left_column + 1).value
             data_table["YER"] = data_temp
     
         if column_name == 'GPS KOORDİNATLARI:':
-            data_temp = ((self.current_worksheet.cell(row = row_idx, column = 2 + 1).value), (self.current_worksheet.cell(row = row_idx, column = 2 + 2).value))
+            data_temp = ((self.current_worksheet.cell(row = row_idx, column = top_left_column + 1).value), (self.current_worksheet.cell(row = row_idx, column = top_left_column + 2).value))
             data_table["GPS KOORDİNATLARI"] = data_temp
     
         if column_name == 'TABLO:':
-            data_temp = self.current_worksheet.cell(row = row_idx, column = 2 + 1).value
+            data_temp = self.current_worksheet.cell(row = row_idx, column = top_left_column + 1).value
             assert(data_temp == None)
             # data_table["TABLO"] = data_temp
     
@@ -110,7 +124,7 @@ class ExcelDataReader(object):
             data = {}
             data[column_name] = []
             for i in range(1, 6):
-                data[column_name].append(self.current_worksheet.cell(row = row_idx, column = 2 + i).value)
+                data[column_name].append(self.current_worksheet.cell(row = row_idx, column = top_left_column + i).value)
             data_table[column_name] = data[column_name]
     
         return 0 # Neither ended nor started
@@ -122,7 +136,7 @@ class ExcelDataReader(object):
         
         data_table = {}
         for row_idx in range(1, row_count + 1):
-            state = self.read_row(row_idx, data_table)
+            state = self.read_row_into_table(row_idx, data_table)
 
             if state == 1: # New table started.
                 table_count += 1
@@ -136,29 +150,43 @@ class ExcelDataReader(object):
 
 # file_path table_name (row_index) param_name System.String value
 def read_and_pickle_2020_data():
-    excel_files_2020 = glob.glob("./data/2020/*.xlsm") # Get a list of 2020 .xlsm files.
-
     total_rowcount = 0
-    data = []
+
+    data_tables = []
     for filepath in excel_files_2020:
         if "Tüm Bölgeler.xlsm" in filepath: break # IMPORTANT: This file includes all the other files' data.
 
         edr = ExcelDataReader(filepath)
-        edr.open_datasheet("RAPOR") # 2020 data only uses this worksheet name.
-
+        edr.open_worksheet("RAPOR") # 2020 data only uses this worksheet name.
         print("Current filename:", edr.filename)
-
         edr.read_tables()
-        
+        data_tables += edr.data_tables
         edr.close()
 
-    print("Total number of tables:", len(edr.data_tables))
+    print("Total number of tables:", len(data_tables))
 
-    dump_to_pickle(data, "tables2020")
+    dump_to_pickle(data_tables, "tables2020")
     print("Pickled the data. Everything done.")
 
+def analyze_2018_data_format():
+    all_row_names = []
+    for filepath in excel_files_2018:
+        edr = ExcelDataReader(filepath)
+        intersections = edr.check_worksheet_names()
+        print("{:60}-> Usable sheets: {}".format(edr.filename, intersections)) # TODO Python's .format() justifying is broken when used with UTF-8 chars. Not that it is important here.
+
+        for ws_name in intersections:
+            print("Worksheet:", ws_name)
+            edr.open_worksheet(ws_name)
+            all_row_names += edr.get_row_names()
+
+    print("All row names:")
+    print(set(all_row_names))
+
 def main():
-    read_and_pickle_2020_data()
+    # read_and_pickle_2020_data()
+    analyze_2018_data_format()
 
 if __name__ == '__main__':
     main()
+
